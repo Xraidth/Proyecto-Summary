e9971de34b426f63a7016d35f46a4feadde2c940
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-08-18 23:56:36 -0300

    schedule 20240818

---
+++ mian.py
@@ -0,0 +1,9 @@
+
+#1) Ingreso de URL de YOUTUBE
+#2) texto = TextExtractor(URL) <-Obtiene el texto
+#FALTA 3) resumen = Sintetizator(texto) <-Obtiene el resumen  
+#4) textTime = TextWithTime(URL) <-Obtiene los tiempos del texto dentro del video
+#FALTA 5) tiempos = Comparator(resumen, textTime) <-Obtiene los tiempos para los recortes
+#FALTA 5) list_clips = Recortador(tiempos) <-Otiene los recortes de video
+#FALTA 6) videoResumen = BinderVideo(list_clips) <-Une los clips en uno
+
